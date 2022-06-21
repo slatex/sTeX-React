919f7c4cee062b8c211e14f358ff0088def06fd1
abchugh <achugh89@gmail.com>
2022-06-21 12:36:06 +0530

    Packages, configs and assets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 450,
+      md: 800,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+  palette: {
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#ffc107',
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}> <Component {...pageProps} /></ThemeProvider>;
 }
 
 export default MyApp
