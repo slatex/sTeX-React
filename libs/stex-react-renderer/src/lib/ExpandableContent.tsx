c38b03545e911b47aa6dfa7e9bde4b53ba4df330
abchugh <achugh89@gmail.com>
2022-06-30 23:19:15 +0530

    Margin of ExpandableContent folds/unfolds content.

--- libs/stex-react-renderer/src/lib/ExpandableContent.tsx
+++ libs/stex-react-renderer/src/lib/ExpandableContent.tsx
@@ -1,7 +1,7 @@
 import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
 import IndeterminateCheckBoxOutlinedIcon from '@mui/icons-material/IndeterminateCheckBoxOutlined';
 import { Box, IconButton } from '@mui/material';
-import { useState } from 'react';
+import { MouseEvent, useState } from 'react';
 import { ContentFromUrl } from './ContentFromUrl';
 
 export function ExpandableContent({
@@ -14,7 +14,8 @@ export function ExpandableContent({
   const [openAtLeastOnce, setOpenAtLeastOnce] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
-  const changeState = () => {
+  const changeState = (e: MouseEvent) => {
+    e.stopPropagation();
     setOpenAtLeastOnce(true);
     setIsOpen((v) => !v);
   };
@@ -27,13 +28,7 @@ export function ExpandableContent({
         sx={{ cursor: 'pointer' }}
         onClick={changeState}
       >
-        <IconButton
-          sx={{ color: 'gray', p: '0' }}
-          onClick={(e) => {
-            e.stopPropagation();
-            changeState();
-          }}
-        >
+        <IconButton sx={{ color: 'gray', p: '0' }} onClick={changeState}>
           {isOpen ? (
             <IndeterminateCheckBoxOutlinedIcon sx={{ fontSize: '20px' }} />
           ) : (
@@ -42,7 +37,7 @@ export function ExpandableContent({
         </IconButton>
         <Box
           sx={{
-            '&:hover': { background: '#DDD' },
+            '& > *:hover': { background: '#DDD' },
             width: 'fit-content',
             px: '4px',
             ml: '-2px',
@@ -54,12 +49,26 @@ export function ExpandableContent({
       </Box>
 
       {openAtLeastOnce && (
-        <Box hidden={!isOpen} ml="9px" pl="10px" borderLeft="1px solid #DDD">
-          {/*The extra margin consumed by each layer is equal to (ml+pl) above */}
-          <ContentFromUrl
-            url={contentUrl}
-            modifyRendered={(bodyNode) => bodyNode?.props?.children}
-          />
+        <Box display={isOpen ? 'flex' : 'none'}>
+          <Box
+            minWidth="20px"
+            sx={{
+              cursor: 'pointer',
+              '&:hover *': { borderLeft: '1px solid #333' },
+            }}
+            onClick={changeState}
+          >
+            <Box width="0" m="auto" borderLeft="1px solid #CCC" height="100%">
+              &nbsp;
+            </Box>
+          </Box>
+          <Box>
+            {/*The extra margin consumed by each layer is equal to (ml+pl) above */}
+            <ContentFromUrl
+              url={contentUrl}
+              modifyRendered={(bodyNode) => bodyNode?.props?.children}
+            />
+          </Box>
         </Box>
       )}
     </Box>
