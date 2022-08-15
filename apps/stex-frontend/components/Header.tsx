704b302482890705d41d2909acef6e9ee6c198d6
abchugh <achugh89@gmail.com>
2022-08-15 22:27:20 +0530

    Login page and Header button

--- apps/stex-frontend/components/Header.tsx
+++ apps/stex-frontend/components/Header.tsx
@@ -1,14 +1,57 @@
-import { Box, Toolbar } from '@mui/material';
+import { Box, Button, Menu, MenuItem, Toolbar } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { isLoggedIn, logout } from '../api/ums';
 import { BrowserAutocomplete } from '../components/BrowserAutocomplete';
 import styles from '../styles/header.module.scss';
+
+function UserButton() {
+  // Menu crap Start
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  // Menu crap End
+  return (
+    <div>
+      <Button
+        sx={{ color: 'white', border: '1px solid white' }}
+        onClick={handleClick}
+      >
+        User
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem
+          onClick={() => {
+            handleClose();
+            logout();
+          }}
+        >
+          Log out
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
 
 export function Header({
   showBrowserAutocomplete,
 }: {
   showBrowserAutocomplete: boolean;
 }) {
+  const loggedIn = isLoggedIn();
+  const router = useRouter();
   return (
     <AppBar position="static">
       <Toolbar className={styles['toolbar']}>
@@ -20,6 +63,22 @@ export function Header({
             <BrowserAutocomplete />
           </Box>
         )}
+        <Box>
+          {loggedIn ? (
+            <UserButton />
+          ) : (
+            <Button
+              sx={{ color: 'white', border: '1px solid white' }}
+              onClick={() => {
+                router.push(
+                  '/login?target=' + encodeURIComponent(window.location.href)
+                );
+              }}
+            >
+              Login
+            </Button>
+          )}
+        </Box>
       </Toolbar>
     </AppBar>
   );
---
+++ apps/stex-frontend/pages/login.tsx
@@ -0,0 +1,82 @@
+import { Box, Button, TextField } from '@mui/material';
+import { BG_COLOR } from '@stex-react/utils';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+
+import {
+  fakeLoginUsingRedirect, isLoggedIn,
+  loginUsingRedirect,
+  logout
+} from '../api/ums';
+import MainLayout from '../layouts/MainLayout';
+
+const LoginPage: NextPage = () => {
+  const loggedIn = isLoggedIn();
+  const router = useRouter();
+  const [fakeId, setFakeId] = useState('1234');
+  const returnBackUrl = router.query.target as string;
+
+  return (
+    <MainLayout>
+      <br />
+      <Box sx={{ mx: '20px' }}>
+        <Box sx={{ m: 'auto', maxWidth: '550px', backgroundColor: BG_COLOR }}>
+          <Box p="3rem" borderRadius="0.3rem" border="1px solid #dee2e6">
+            <br />
+            <TextField
+              label="FakeId"
+              value={fakeId}
+              onChange={(e) => setFakeId(e.target.value)}
+              sx={{ my: '10px' }}
+            />
+            {loggedIn ? (
+              <Button
+                fullWidth
+                variant="contained"
+                size="large"
+                sx={{ w: '100%' }}
+                onClick={() => logout()}
+              >
+                Logout
+              </Button>
+            ) : (
+              <>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  size="large"
+                  sx={{ fontSize: '32x' }}
+                  onClick={() => {
+                    if (fakeId) fakeLoginUsingRedirect(fakeId, returnBackUrl);
+                    else loginUsingRedirect(returnBackUrl);
+                  }}
+                >
+                  Login through FAU IdM-Portal
+                </Button>
+                <span
+                  style={{
+                    fontFamily: 'Roboto',
+                    color: '#888',
+                    marginTop: '10px',
+                  }}
+                >
+                  <hr />
+                  Please remember to logout after you are done.
+                  <br />
+                  <br />
+                  <i style={{ color: 'red' }}>
+                    Warning: Logging out from FAU IdM-Portal will not log you
+                    out here.
+                  </i>
+                </span>
+              </>
+            )}
+          </Box>
+        </Box>
+      </Box>
+    </MainLayout>
+  );
+};
+
+export default LoginPage;
