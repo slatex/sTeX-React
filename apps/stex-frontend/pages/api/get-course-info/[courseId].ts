d62c95821928f6bef094964c1aadcf025ce4c974
abchugh <achugh89@gmail.com>
2022-09-11 22:29:27 +0530

    ...

--- apps/stex-frontend/pages/api/get-course-info/[courseId].ts
+++ apps/stex-frontend/pages/api/get-course-info/[courseId].ts
@@ -12,7 +12,9 @@ const COURSE_INFO_CACHE = new Map<string, CourseInfo>();
 export default async function handler(req, res) {
   const { courseId } = req.query;
 
-  if (COURSE_INFO_CACHE.has(courseId)) return COURSE_INFO_CACHE[courseId];
+  if (COURSE_INFO_CACHE.has(courseId)) {
+    return res.status(200).json(COURSE_INFO_CACHE[courseId]);
+  }
 
   if (courseId !== 'ai-1') {
     res.status(404).json({ error: 'Course not found!' });
