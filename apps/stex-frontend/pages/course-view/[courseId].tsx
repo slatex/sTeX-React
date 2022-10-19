ddf762d8d1ba961a440bf635ca9072582a8ecaf5
abchugh <achugh89@gmail.com>
2022-10-19 23:44:56 +0530

    ...

--- apps/stex-frontend/pages/course-view/[courseId].tsx
+++ apps/stex-frontend/pages/course-view/[courseId].tsx
@@ -18,7 +18,7 @@ import { SlideDeckNavigation } from '../../components/SlideDeckNavigation';
 import { TooltipToggleButton } from '../../components/TooltipToggleButton';
 import { VideoDisplay } from '../../components/VideoDisplay';
 import MainLayout from '../../layouts/MainLayout';
-import { CourseInfo, DeckAndVideoInfo } from '../../shared/slides';
+import { CourseInfo, DeckAndVideoInfo, Slide } from '../../shared/slides';
 
 function RenderElements({ elements }: { elements: string[] }) {
   return (
