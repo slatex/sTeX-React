b7a414770fbb03f40b59c9c80257b04766fe7b36
Jonas Betzendahl <jonas.betzendahl@fau.de>
2023-03-30 16:43:41 +0200

    add students paper prototype

---
+++ apps/stex-frontend/pages/exp/pp_students.tsx
@@ -0,0 +1,30 @@
+import { Box } from '@mui/material';
+import axios from 'axios';
+import parse from 'html-react-parser';
+import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+import MainLayout from '../../layouts/MainLayout';
+
+function customReplaceFunc(d: any) {
+  if (d.name === 'img' && d['attribs']['id'] === 'lernkurve')
+    return <CycleImage />;
+  if (d.name === 'body') d.name = 'div';
+}
+
+const PPTeachersAndTas: NextPage = () => {
+  const [renderedHtml, setRenderedHtml] = useState<any>(undefined);
+  useEffect(() => {
+    axios.get('/exp_res/pp_students.htm').then((resp) => {
+      setRenderedHtml(parse(resp.data, { replace: customReplaceFunc }));
+    });
+  }, []);
+  return (
+    <MainLayout title="Experiments | VoLL-KI">
+      <Box px="10px" m="auto" maxWidth="650px">
+        {renderedHtml}
+      </Box>
+    </MainLayout>
+  );
+};
+
+export default PPTeachersAndTas;
