f7f5fe8e7c1f86efa10e4781c7010d1922e98ab1
AbhishekChugh <achugh89@gmail.com>
2023-10-31 23:06:54 +0530

    ...

--- packages/api/src/lib/quiz.ts
+++ packages/api/src/lib/quiz.ts
@@ -60,6 +60,7 @@ export interface UserResponse {
 export interface QuizStatsResponse {
   attemptedHistogram: { [attempted: number]: number };
   scoreHistogram: { [score: number]: number };
+  timeHistogram: { [ts: number]: number };
 }
 
 // For recording quizzes at /quiz/old
