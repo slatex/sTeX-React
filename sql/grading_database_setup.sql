43e96f7c3c098e424cdf33a3aa426a5c9caf0c7f
AbhishekChugh <achugh89@gmail.com>
2023-10-31 15:33:30 +0530

    table update

--- sql/grading_database_setup.sql
+++ sql/grading_database_setup.sql
@@ -9,11 +9,11 @@ CREATE TABLE grading (
     quizId varchar(255) NOT NULL,
     problemId varchar(255) NOT NULL,
     
-    singleOptionIdx int,
+    singleOptionIdxs varchar(255), # comma delimited list of numbers
     multipleOptionIdxs varchar(255), # comma delimited list of numbers
     filledInAnswer text,
     
-    isCorrect boolean,
+    points float,
     
     browserTimestamp_ms bigint,
     postedTimestamp timestamp DEFAULT CURRENT_TIMESTAMP
