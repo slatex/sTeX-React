f8a8ee3271c282ac3a886cc89744f8178fbd3f76
behrooz bozorg chami <behroozbc@outlook.com>
2024-06-14 18:40:10 +0200

    add some flattening dep

--- packages/alea-frontend/pages/api/acl-utils/flattening.ts
+++ packages/alea-frontend/pages/api/acl-utils/flattening.ts
@@ -3,45 +3,35 @@ import { executeAndEndSet500OnError } from "../comment-utils";
 import { addSet, getSet } from "./redis-connection-utils";
 
 export class Flattening {
-    private _aclMemberWantToSearch: Set<string> = new Set<string>();
-    private _aclMemberlisted: string[] = [];
     private _aclsWantToSearch: Set<string> = new Set<string>();
     private _aclslisted: string[] = [];
+    private readonly _membersSavePostfix = 'members';
+    private readonly _aclsSavePostfix = 'acls';
     constructor (private res: NextApiResponse) {
     }
     public async findMembers(AclId: string): Promise<string[]> {
         const output: Set<string> = new Set<string>();
-        if (this._aclMemberlisted.includes(AclId))
-            return (await getSet(AclId)) as string[];
-        const members = await executeAndEndSet500OnError<ACLMembership[]>('select * from ACLMembership where parentACLId=?', [AclId], this.res);
+        const redisName = `${AclId}-${this._membersSavePostfix}`;
+        const allAcls = await this.findACL(AclId);
+        allAcls.push(AclId);
+        const members = await executeAndEndSet500OnError<ACLMembership[]>('select * from ACLMembership where parentACLId in (?) AND memberUserId is not null', [allAcls], this.res);
         for (const member of members.filter(c => c.memberUserId)) {
             output.add(member.memberUserId);
         }
-        const acls = members.filter(c => c.memberACLId);
-        for (const acl of acls) {
-            if (!this._aclMemberWantToSearch.has(AclId)) {// break deepend on each other loop
-                if (!this._aclMemberlisted.includes(AclId))
-                    this._aclMemberWantToSearch.add(AclId);
-                for (const treeMember of (await this.findMembers(acl.memberACLId))) {
-                    output.add(treeMember);
-                }
-            }
-        }
-        this._aclMemberWantToSearch.delete(AclId);
-        this._aclMemberlisted.push(AclId);
         if (output.size != 0)
-            await addSet(AclId, Array.from(output));
+            await addSet(redisName, Array.from(output));
         return Array.from(output);
     }
     public async findACL(AclId: string) {
         const output: Set<string> = new Set<string>();
-        if (this._aclMemberlisted.includes(AclId))
-            return (await getSet(`${AclId}-acl`)) as string[];
+        const redisName = `${AclId}-${this._aclsSavePostfix}`;
+        if (this._aclslisted.includes(AclId))
+            return (await getSet(redisName)) as string[];
         const acls = await executeAndEndSet500OnError<ACLMembership[]>('select * from ACLMembership where parentACLId=? AND memberUserId is null', [AclId], this.res);
         for (const acl of acls) {
-            if (!this._aclsWantToSearch.has(AclId)) {// break deepend on each other loop
-                if (!this._aclslisted.includes(AclId))
-                    this._aclsWantToSearch.add(AclId);
+            output.add(acl.memberACLId);
+            if (!this._aclsWantToSearch.has(acl.memberACLId)) {// break deepend on each other loop
+                this._aclsWantToSearch.add(acl.memberACLId);
                 for (const treeMember of (await this.findACL(acl.memberACLId))) {
                     output.add(treeMember);
                 }
@@ -50,7 +40,7 @@ export class Flattening {
         this._aclsWantToSearch.delete(AclId);
         this._aclslisted.push(AclId);
         if (output.size != 0)
-            await addSet(AclId, Array.from(output));
+            await addSet(redisName, Array.from(output));
         return Array.from(output);
     }
 };
