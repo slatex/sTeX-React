a35c7860555b991c0028024daa9bc38b91ba5036
behrooz bozorg chami <behroozbc@outlook.com>
2024-07-01 14:26:00 +0200

    is member

---
+++ packages/alea-frontend/pages/api/access-control/is-member.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { checkIfGetOrSetError } from "../comment-utils";
+import { isMemberOfAcl } from "../acl-utils/acl-commen-utils";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (!checkIfGetOrSetError(req, res)) return;
+    const { id, userId } = req.query;
+    if (!id || !userId) return res.status(422).send(`Missing param(s).`);
+    res.send(await isMemberOfAcl(id as string, userId as string));
+}
