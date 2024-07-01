e655f33201ea486f85bac332c8d52d6bb6faba08
behrooz bozorg chami <behroozbc@outlook.com>
2024-07-01 14:27:12 +0200

    is valid acl for ui

---
+++ packages/alea-frontend/pages/api/access-control/is-valid.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { checkIfGetOrSetError, executeAndEndSet500OnError } from "../comment-utils";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (!checkIfGetOrSetError(req, res)) return;
+    const id = req.query.id as string;
+    if (!id) return res.status(422).send(`Missing param id.`);
+    const result=await executeAndEndSet500OnError('SELECT COUNT(id) FROM AccessControlList WHERE id =?',[id],res)
+    res.send(result[0]["COUNT(id)"]===1);
+}
