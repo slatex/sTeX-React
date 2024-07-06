32463035078149fde3caef311da34fb802a7ed79
behrooz bozorg chami <behroozbc@outlook.com>
2024-07-06 17:06:07 +0200

    change is valid query

--- packages/alea-frontend/pages/api/access-control/is-valid.ts
+++ packages/alea-frontend/pages/api/access-control/is-valid.ts
@@ -8,6 +8,6 @@ export default async function handler(
     if (!checkIfGetOrSetError(req, res)) return;
     const id = req.query.id as string;
     if (!id) return res.status(422).send(`Missing param id.`);
-    const result=await executeAndEndSet500OnError('SELECT COUNT(id) FROM AccessControlList WHERE id =?',[id],res)
-    res.send(result[0]["COUNT(id)"]===1);
+    const result=await executeAndEndSet500OnError<[]>('SELECT id FROM AccessControlList WHERE id =?',[id],res)
+    res.send(result.length>0);
 }
