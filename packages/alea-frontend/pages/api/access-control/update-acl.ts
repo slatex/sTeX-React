28aab8677511446483f2b37aec1fe04e1dddb3dd
behrooz bozorg chami <behroozbc@outlook.com>
2024-07-09 23:02:02 +0200

    check updater of acl

--- packages/alea-frontend/pages/api/access-control/update-acl.ts
+++ packages/alea-frontend/pages/api/access-control/update-acl.ts
@@ -2,22 +2,21 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import {
   checkIfPostOrSetError,
   executeAndEndSet500OnError,
-  getUserIdOrSetError,
 } from '../comment-utils';
+import { isCurrentUserMemberOfAClupdater } from '../acl-utils/acl-common-utils';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (!checkIfPostOrSetError(req, res)) return;
-  const userId = getUserIdOrSetError(req, res);
-  if (!userId) return;
 
   const { id, description, isOpen, updaterACLId } = req.body;
   if (!id || !description || isOpen || !updaterACLId) {
     return res.status(422).send('Missing required fields.');
   }
-
+  if (!await isCurrentUserMemberOfAClupdater(id, res, req))
+    return res.status(403).end();
   await executeAndEndSet500OnError(
     'UPDATE AccessControlList SET description=?, updaterACLId=?, isOpen=? WHERE id=?',
     [description, updaterACLId, !!isOpen, id],
