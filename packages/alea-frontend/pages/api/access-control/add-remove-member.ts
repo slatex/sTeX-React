b86d71ae004d33c740729c6e213f052a6ab4123a
behrooz bozorg chami <behroozbc@outlook.com>
2024-07-09 23:48:45 +0200

    add check for delete and instert

--- packages/alea-frontend/pages/api/access-control/add-remove-member.ts
+++ packages/alea-frontend/pages/api/access-control/add-remove-member.ts
@@ -15,21 +15,24 @@ export default async function handler(
   const aclId = req.body.memberId as string;
   const isAclMember = req.body.isAclMember as boolean;
   const toBeAdded = req.body.toBeAdded as boolean;
-  
+
   if (!aclId || !memberId || isAclMember === null || toBeAdded === null) {
     return res.status(422).send('Missing fields.');
   }
   const acl: AccessControlList = await executeAndEndSet500OnError('select * from AccessControlList where id=?', [aclId], res)[0];
-  if (!(acl.isOpen || await isCurrentUserMemberOfAClupdater(aclId,res,req)))
-    return res.status(403).end();
+
   // check if in updaterACL or (1) isOpen for self-additions (2) is self deletion
   let query = '';
   let params: string[] = [];
   if (toBeAdded) {
+    if (!(acl.isOpen || await isCurrentUserMemberOfAClupdater(aclId, res, req)))
+      return res.status(403).end();
     query =
       'INSERT INTO ACLMembership (parentACLId, memberACLId, memberUserId) VALUES (?, ?, ?)';
     params = isAclMember ? [aclId, memberId, null] : [aclId, null, memberId];
   } else {
+    if (! await isCurrentUserMemberOfAClupdater(aclId, res, req))
+      return res.status(403).end();
     const memberField = isAclMember ? 'memberACLId' : 'memberUserId';
     query = `DELETE FROM ACLMembership WHERE parentACLId=? AND ${memberField} = ?`;
     params = [aclId, memberId];
