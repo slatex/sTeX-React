4afcf041fd2c6df0c15fac0232230319a015e3f7
behrooz bozorgchamy <behroozbc@outlook.com>
2024-09-02 18:57:58 +0330

    nap types

--- packages/api/src/lib/nap.ts
+++ packages/api/src/lib/nap.ts
@@ -7,3 +7,21 @@ export interface AnswerClass {
   description: string; //from API
   count: number;
 }
+export interface CreateGrading {
+  customFeedback: string;
+  answerId: number;
+  answerClasses: AnswerClass[];
+}
+export interface CreateAnswer {
+  answer: string;
+  questionId: string;
+  question_title: string;
+}
+export interface CreateReviewRequest {
+  answerId: number;
+  reviewType: ReviewType;
+}
+export enum ReviewType {
+  PEER = 'Peer',
+  INSTRUCTOR = 'INSTRUCTOR',
+}
