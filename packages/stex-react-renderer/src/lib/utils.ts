7f6c9565929d48586cfd0b2b2e48d78a748e1ade
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2024-09-03 18:49:21 +0530

    fix the view source and refactored the check solution functionality

--- packages/stex-react-renderer/src/lib/utils.ts
+++ packages/stex-react-renderer/src/lib/utils.ts
@@ -1,9 +1,9 @@
 export function extractProjectIdAndFilepath(problemId: string) {
-    const url = problemId
-      .replace('http://mathhub.info/', '')
-      .replace(/\?en.*/, '');
-    const parts = url.split('/');
-    const projectId = parts[0] + '/' + parts[1];
-    const filePath = parts.slice(2).join('/').replace('.omdoc', '.tex');
-    return [projectId, filePath];
-  }
+  const url = problemId.replace('http://mathhub.info/', '').replace(/\?en.*/, '');
+  const parts = url.split('/');
+  const defaultProjectParts = 4;
+  const projectParts = Math.min(defaultProjectParts, parts.length - 2);
+  const projectId = parts.slice(0, projectParts).join('/');
+  const filePath = parts.slice(projectParts).join('/').replace('.omdoc', '.tex');
+  return [projectId, filePath];
+}
