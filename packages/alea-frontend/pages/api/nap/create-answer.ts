28f88e9a090db9fb32f418071d03dc4190a6096a
behrooz bozorgchamy <behroozbc@outlook.com>
2024-09-08 16:21:51 +0330

    fix

--- packages/alea-frontend/pages/api/nap/create-answer.ts
+++ packages/alea-frontend/pages/api/nap/create-answer.ts
@@ -4,16 +4,17 @@ import {
   executeAndEndSet500OnError,
   getUserIdOrSetError,
 } from '../comment-utils';
+import { CreateAnswer } from '@stex-react/api';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!checkIfPostOrSetError(req, res)) return;
   const userId = await getUserIdOrSetError(req, res);
-  const [answer, questionId, question_title] = req.body;
+  const { answer,questionId, question_title } = req.body as CreateAnswer;
   if (!answer || !questionId || !question_title) res.status(422).end();
   const result = await executeAndEndSet500OnError(
     `INSERT INTO Answer (questionId, userId, answer,question_title) VALUES (?,?,?,?)`,
     [questionId, userId, answer, question_title],
     res
   );
-  res.status(201).end({ id: result['id'] });
+  res.status(201).send({ id: result.insertId });
 }
