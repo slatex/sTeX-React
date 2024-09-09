b6109578d76d3b1726e917aa759f6a920fc6d32d
behrooz bozorgchamy <behroozbc@outlook.com>
2024-09-09 11:59:28 +0330

    delete answer

---
+++ packages/alea-frontend/pages/api/nap/delete-answer.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import {
+  checkIfPostOrSetError,
+  executeAndEndSet500OnError,
+  getUserIdOrSetError,
+} from '../comment-utils';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (!checkIfPostOrSetError(req, res)) return;
+  const userId = await getUserIdOrSetError(req, res);
+  const { id } = req.body;
+  await executeAndEndSet500OnError(`Delete From Answer Where userId=? and id=?`, [userId, id], res);
+  res.status(200).end()
+}
