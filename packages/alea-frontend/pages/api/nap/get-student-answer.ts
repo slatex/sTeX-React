7ad7554e3246a2dd71609b1e0527e9830596c8e3
behrooz bozorgchamy <behroozbc@outlook.com>
2024-09-13 18:16:16 +0330

    add todo

--- packages/alea-frontend/pages/api/nap/get-student-answer.ts
+++ packages/alea-frontend/pages/api/nap/get-student-answer.ts
@@ -5,6 +5,7 @@ import { AnswerResponse, AnswerClassResponse, GradeResponse } from '@stex-react/
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!checkIfGetOrSetError(req, res)) return;
   const id = req.query.id as string;
+  //TODO: Adding acl. this api should return the answer only for the student him/herself or instructors. 
   const answer = (
     await executeAndEndSet500OnError<AnswerResponse[]>(`select * from Answer where id=?`, [id], res)
   )[0];
@@ -15,7 +16,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   );
   const gradesAnswerClasses = await executeAndEndSet500OnError<AnswerClassResponse[]>(
     'select * from GradingAnswerClass where gradingId in (?)',
-    [grades.flatMap((c) => c.id)],
+    [grades.map((c) => c.id)],
     res
   );
   for (const grade of grades) {
