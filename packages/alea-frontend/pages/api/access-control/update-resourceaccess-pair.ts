79785890f53b69a49fd485bafb1b520294eb9bd9
abchugh <achugh89@gmail.com>
2024-10-15 15:34:14 +0530

    Bug fixes

--- packages/alea-frontend/pages/api/access-control/update-resourceaccess-pair.ts
+++ packages/alea-frontend/pages/api/access-control/update-resourceaccess-pair.ts
@@ -4,18 +4,22 @@ import {
   executeAndEndSet500OnError,
   getUserIdOrSetError,
 } from '../comment-utils';
-import { isMemberOfAcl } from '../acl-utils/acl-common-utils';
 import { canUpdateAccessControlEntries } from './resource-utils';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!checkIfPostOrSetError(req, res)) return;
   const { aclId, resourceId, actionId } = req.body;
   const userId = await getUserIdOrSetError(req, res);
-  if (!aclId || !resourceId || !actionId) res.status(422).send(`Missing params.`);
+  if (!aclId || !resourceId || !actionId) return res.status(422).send('Missing params.');
   if (!(await canUpdateAccessControlEntries(res, resourceId, userId)))
     return res.status(403).send('unauthorized');
-  const query = `UPDATE ResourceAccess SET aclId = ? WHERE resourceId = ? and actionId = ?`;
+
+  const query = `
+    INSERT INTO ResourceAccess (aclId, resourceId, actionId)
+    VALUES (?, ?, ?)
+    ON DUPLICATE KEY UPDATE aclId = VALUES(aclId)
+  `;
   const result = await executeAndEndSet500OnError(query, [aclId, resourceId, actionId], res);
   if (!result) return;
-  res.status(200).send({ message: 'updated' });
+  res.status(204).end();
 }
