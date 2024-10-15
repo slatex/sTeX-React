617c44050f3ebed9b1076a34fc313a7b5bd561f9
abchugh <achugh89@gmail.com>
2024-10-15 14:28:51 +0530

    Allow students to get course homework

--- packages/alea-frontend/pages/api/homework/get-homework.ts
+++ packages/alea-frontend/pages/api/homework/get-homework.ts
@@ -1,21 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { checkIfGetOrSetError, executeDontEndSet500OnError } from '../comment-utils';
-import { getUserIdIfAuthorizedOrSetError } from '../access-control/resource-utils';
-import { Action, ResourceName } from '@stex-react/utils';
-import { CURRENT_TERM } from '@stex-react/utils';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!checkIfGetOrSetError(req, res)) return;
   const courseId = req.query.courseId as string;
   if (!courseId) return res.status(422).send({ messge: `Missing params.` });
-  const userId = await getUserIdIfAuthorizedOrSetError(
-    req,
-    res,
-    ResourceName.COURSE_HOMEWORK,
-    Action.MUTATE,
-    { courseId, instanceId: CURRENT_TERM }
-  );
-  if (!userId) return;
   const results = await executeDontEndSet500OnError(
     'SELECT * FROM homework WHERE courseId = ? ORDER BY homeworkGivenDate ASC',
     [courseId],
