2ebdc34103ae1fcc904047aed0112590d04a4f4b
abchugh <achugh89@gmail.com>
2024-10-16 15:49:01 +0530

    flattening fixes

--- packages/alea-frontend/pages/api/acl-utils/flattening.ts
+++ packages/alea-frontend/pages/api/acl-utils/flattening.ts
@@ -19,35 +19,37 @@ export class Flattening {
     for (const member of members.filter((c) => c.memberUserId)) {
       output.add(member.memberUserId);
     }
-    if (output.size != 0) await this._cacheStore.addToSet(keyName, Array.from(output));
+    if (output.size !== 0) await this._cacheStore.addToSet(keyName, Array.from(output));
     return Array.from(output);
   }
-  public async findACL(AclId: string): Promise<string[]> {
+  public async findACL(aclId: string): Promise<string[]> {
+    const keyName = getCacheKey(aclId, AclSavePostfix.acl);
+    if (this._alreadyComputed.includes(aclId))
+      return (await this._cacheStore.getFromSet(keyName)) as string[];
+
     const output: Set<string> = new Set<string>();
-    const KeyName = getCacheKey(AclId, AclSavePostfix.acl);
-    if (this._alreadyComputed.includes(AclId))
-      return (await this._cacheStore.getFromSet(KeyName)) as string[];
-    const acls = this._aclMembership.filter((c) => c.parentACLId && !c.memberUserId);
+    const acls = this._aclMembership.filter((c) => c.parentACLId === aclId && !c.memberUserId);
     for (const acl of acls) {
       output.add(acl.memberACLId);
+
+      // don't recurse if there is a depend-on-each-other loop
       if (!this._ancestorChain.has(acl.memberACLId)) {
-        // break deepend on each other loop
         this._ancestorChain.add(acl.memberACLId);
         for (const treeMember of await this.findACL(acl.memberACLId)) {
           output.add(treeMember);
         }
       }
     }
-    this._ancestorChain.delete(AclId);
-    this._alreadyComputed.push(AclId);
-    if (output.size != 0) await this._cacheStore.addToSet(KeyName, Array.from(output));
+    this._ancestorChain.delete(aclId);
+    this._alreadyComputed.push(aclId);
+    if (output.size !== 0) await this._cacheStore.addToSet(keyName, Array.from(output));
     return Array.from(output);
   }
 }
+
 export interface ACLMembership {
   id: number;
   memberUserId: string;
   memberACLId: string;
   parentACLId: string;
 }
-s
