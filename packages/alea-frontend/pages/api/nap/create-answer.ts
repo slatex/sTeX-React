dc9d5f0638ccd8c49b1fa66797117a6c46bece95
behrooz bozorg chami <behroozbc@outlook.com>
2024-10-19 11:44:55 +0200

    add courseInstance

--- packages/alea-frontend/pages/api/nap/create-answer.ts
+++ packages/alea-frontend/pages/api/nap/create-answer.ts
@@ -16,8 +16,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (!courseInstance) courseInstance = 'current';
   answer = answer.trim();
   const result = await executeAndEndSet500OnError(
-    `INSERT INTO Answer (questionId, userId, answer,questionTitle,subProblemId,courseId,courseInstance) VALUES (?,?,?,?,?,?)`,
-    [questionId, userId, answer, questionTitle, subProblemId, courseId,courseInstance],
+    `INSERT INTO Answer (questionId, userId, answer,questionTitle,subProblemId,courseId,courseInstance) VALUES (?,?,?,?,?,?,?)`,
+    [questionId, userId, answer, questionTitle, subProblemId, courseId, courseInstance],
     res
   );
   res.status(201).send({ id: result.insertId });
