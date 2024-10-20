599479833d505524fce51ef807a3d66fb41051be
abchugh <achugh89@gmail.com>
2024-10-20 20:35:03 +0530

    fix

--- packages/alea-frontend/pages/api/study-buddy/get-courses-sortedby-connections.ts
+++ packages/alea-frontend/pages/api/study-buddy/get-courses-sortedby-connections.ts
@@ -3,6 +3,7 @@ import { CURRENT_TERM } from '@stex-react/utils';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getUserIdIfCanModerateStudyBuddyOrSetError } from '../access-control/resource-utils';
 import { executeAndEndSet500OnError } from '../comment-utils';
+import { getCourseIdAndInstanceFromSbCourseId } from './study-buddy-utils';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const userId = await getUserIdIfCanModerateStudyBuddyOrSetError(req, res);
@@ -11,14 +12,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   let instanceId = req.query.instanceId as string;
   if (!instanceId) instanceId = CURRENT_TERM;
 
-  const result: GetSortedCoursesByConnectionsResponse[] = await executeAndEndSet500OnError(
-    `SELECT COUNT(courseId) as member, courseId
+  const results: any[] = await executeAndEndSet500OnError(
+    `SELECT COUNT(sbCourseId) as member, sbCourseId
         FROM StudyBuddyUsers
         WHERE sbCourseId LIKE '%${instanceId}'
-        GROUP BY courseId
+        GROUP BY sbCourseId
         ORDER BY member DESC`,
     [],
     res
   );
-  res.status(200).json(result);
+  const allCoursesInfo: GetSortedCoursesByConnectionsResponse[] = results.map((r) => ({
+    courseId: getCourseIdAndInstanceFromSbCourseId(r.sbCourseId).courseId,
+    member: r.member,
+  }));
+  res.status(200).json(allCoursesInfo);
 }
--- packages/alea-frontend/pages/api/study-buddy/get-enrolled-course-ids.ts
+++ packages/alea-frontend/pages/api/study-buddy/get-enrolled-course-ids.ts
@@ -5,6 +5,7 @@ import {
 } from '../comment-utils';
 import { EnrolledCourseIds } from '@stex-react/api';
 import { CURRENT_TERM } from '@stex-react/utils';
+import { getCourseIdAndInstanceFromSbCourseId } from './study-buddy-utils';
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,12 +17,16 @@ export default async function handler(
   let instanceId = req.query.instanceId as string;
   if (!instanceId) instanceId = CURRENT_TERM;
 
-  const result: EnrolledCourseIds[] = await executeAndEndSet500OnError(
-    `SELECT courseId, active as activeStatus from StudyBuddyUsers 
+  const results: any[] = await executeAndEndSet500OnError(
+    `SELECT sbCourseId, active as activeStatus from StudyBuddyUsers 
     WHERE sbCourseId LIKE '%${instanceId}' AND userId=?`,
     [userId],
     res
   );
+  const enrolledCourseIds: EnrolledCourseIds[] = results.map((r) => ({
+    courseId: getCourseIdAndInstanceFromSbCourseId(r.sbCourseId).courseId,
+    activeStatus: r.activeStatus,
+  }));
 
-  res.status(200).json(result);
+  res.status(200).json(enrolledCourseIds);
 }
