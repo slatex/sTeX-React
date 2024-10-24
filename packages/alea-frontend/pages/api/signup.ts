843f459e408b7c0076b75561d5af44a120a1ece9
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2024-10-24 17:02:52 +0530

    signup issue fixed

--- packages/alea-frontend/pages/api/signup.ts
+++ packages/alea-frontend/pages/api/signup.ts
@@ -26,7 +26,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (email.endsWith('@fau.de')) {
     return res.status(400).json({ message: "Can't use this email" });
   }
-  if (!(await doesUserIdExist(email, res))) {
+  if (await doesUserIdExist(email, res)) {
     return res.status(400).send('User already exists');
   }
 
