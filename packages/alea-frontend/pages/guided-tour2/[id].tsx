fb3d0a73c567d174a06b29a427d10b078b280a6c
Abhishek Kumar <abhishek2021005@gmail.com>
2024-11-13 09:25:35 +0530

    get Snapshot bug
    Signed-off-by: Abhishek Kumar <abhishek2021005@gmail.com>

--- packages/alea-frontend/pages/guided-tour2/[id].tsx
+++ packages/alea-frontend/pages/guided-tour2/[id].tsx
@@ -1,188 +1,190 @@
-import { getLeafConcepts, getLearningObjects, getProblemShtml } from '@stex-react/api';
+import { getLeafConcepts, getLearningObjects } from '@stex-react/api';
 import MainLayout from 'packages/alea-frontend/layouts/MainLayout';
-import React, { useState, useEffect, useRef, useContext } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import { useRouter } from 'next/router';
-import styles from './guided-tour.module.scss'; // Import CSS module for styling
+import styles from './guided-tour.module.scss';
 import { Box, Button, Typography } from '@mui/material';
 import { ServerLinksContext } from '@stex-react/stex-react-renderer';
+import { useMachine } from '@xstate/react';
+import { createMachine, assign } from 'xstate';
+
+const tourMachine = createMachine(
+  {
+    /** @xstate-layout N4IgpgJg5mDOIC5RQK4EsKQCoHsUCcA6NAOzQBc0BDAGzQC9SoBiCHEsYkgNxwGtOqDNjxFSFanUYkoCUrwDGVSuwDaABgC6GzYlAAHHLAns9IAB6IATAE4AjIXUB2ABx2rAZgAsHp54CsAGyBADQgAJ6I-l4ujuo2vsF+wTb+AL5pYUKYELgEXBK0DEzMYPj4OET6NMoAZpUAtoTZIvnilEXSsvI4SiokOjpmhsb9ZpYItg7Obp4+fh5BoRGITupehDH+Nl5W-i4eVuouVhlZ6Dl5RLVg5AoAFkwAMmBU+GQyAPIARgBWYApyLBWOxOD0BM0Lq1rrcHs9Xu8mD9-oDYHIeL1lGg1FohkgQCMTCRxqs3IQXCcnKkPAl1FYvE4wpEEF59uT1IE9hybEcPHYziAWrlRIQbndHjIXm8PlBkQCgaVypVCNU6o1IcJhfkxXDJQiZXLUejFFicdotMMjESSQhXA4KVYqf4aR46QymVFOYQ9glXIE7DsTl4BUKroQqLA+EwAMI4Br1fDkZgAJQAogBlAAKnwAcunUwB9ACaGbxBitY3xE3cTn8hCC9J5rqcVN8HsmVishBsTkCDb8NniLl7IahWqIEajMlj8cqSbTWdz+YLOc+ZYJFexxKriBrdYbXibzlbjJWLMCHkIdgb-k8gQpDKcHlHmrDk5jcYT84z2bzhdXWAFumACqabroSlagNWjr7pyh6eMevrtmsl4eIsOxHMc2yBE4L6XCKsD3DgADuTAACJgLU7RbimP5LoWJbpuBm6mDuCB2IECTdoE8T+O4xy7HYXjth4wSECh7jbLs3guF4waZIKY5hoRJHkZR1HsLRi5-iua4WviEFbja142DYhA0nJT6yTh96nsyPF1oO-h0ocgReDM8nnK+BFEaRMiZhU3w0GADRab+y6McxoxGWxRwuOo3G8XYzjqGhHjtpxl5OMlCQ2Jxjqsnh0KECpflQAFOBBSFYX0bpUXWmxAbxJsXhCbYvY0k4XjLMyDJmbJ2V+PEviOqcCmhiKHDmOQsYkAoYD6Em9WQRY1jHA4MRyeoQktp4LjtmSh6mTYByDnYiypUV46EFNM3sPNi3MKodi6AZLHblB1isglPHXg6uypH2yEcuSw7OXS6zeGNCkkDgmDwPiE0EJa0WsZ9CAALQ9YgGOOcd-i1g6dhuDsV1htRnRMCjDXo7sGUUuJ7iHmDeUnGTIo6hKUBSoiXx-PKCPlqjH2rQgfGxJJ6jbZtrV2VE3WbC42zeE6gPPuNSkiu+06fnO1MrdWxyXt1QR8oGVgce2MQJa4VhuFL9LqM66tefh+SlWpVFkCthlo6L16peZg5uU7fVoftZ4xIEhAXnbOXOS6qTs+7vlMBVVUNPrMXo01+40sN51WDx9LIXYThXsOzMFVM-Ia95+S3bND3kFnfsTAhXbOoOPKmayQnA3W51dakh63pxydEAocbVLckCtyL7f3tHbkDU+3XZW2Z73glHhg7sFL3qynmKfXRBlBU+Dz8ZLkx8NtapesrUR-ZLjRxbewXkEUs9oEGQZEAA */
+    id: 'guidedTour',
+    initial: 'initializing',
+    context: {
+      conceptData: [],
+      currentConceptIndex: 0,
+      messages: [],
+      mmtUrl: '',
+    },
+    states: {
+      initializing: {
+        invoke: {
+          src: 'fetchLeafConcepts',
+          onDone: { target: 'fetchingLearningObjects', actions: 'storeConcepts' },
+          onError: { target: 'error', actions: 'sendErrorMessage' },
+        },
+      },
+      fetchingLearningObjects: {
+        invoke: {
+          src: 'fetchLearningObjects',
+          onDone: { target: 'askingComfort', actions: 'promptFirstConcept' },
+          onError: { target: 'error', actions: 'sendErrorMessage' },
+        },
+      },
+      askingComfort: {
+        on: {
+          RESPONSE_YES: { target: 'nextConcept', actions: 'moveToNextConcept' },
+          RESPONSE_NO: { target: 'showingDefinition', actions: 'showDefinition' },
+          RESPONSE_NOT_SURE: { target: 'showingProblem', actions: 'showProblem' },
+        },
+      },
+      showingDefinition: {
+        on: {
+          RESPONSE_YES: { target: 'nextConcept', actions: 'moveToNextConcept' },
+          RESPONSE_NO: { target: 'showingProblem', actions: 'showProblem' },
+        },
+      },
+      showingProblem: {
+        on: {
+          RESPONSE_YES: { target: 'nextConcept', actions: 'moveToNextConcept' },
+          RESPONSE_NO: { target: 'showingDefinition', actions: 'showDefinition' },
+        },
+      },
+      nextConcept: {
+        always: [
+          { target: 'askingComfort', guard: 'hasNextConcept', actions: 'promptNextConcept' },
+          { target: 'completed', guard: 'noMoreConcepts' },
+        ],
+      },
+      completed: {
+        type: 'final',
+        entry: 'completeTour',
+      },
+      error: {
+        type: 'final',
+        entry: 'sendErrorMessage',
+      },
+    },
+  },
+  {
+    actions: {
+      storeConcepts: assign({
+        conceptData: (context, event) => event.data,
+      }),
+      promptFirstConcept: assign({
+        messages: (context) => [
+          ...context.messages,
+          {
+            text: `Do you feel comfortable with ${context.conceptData[0]?.title}?`,
+            type: 'system',
+          },
+        ],
+      }),
+      showDefinition: assign({
+        messages: (context) => [
+          ...context.messages,
+          {
+            text: `Here's the definition of ${
+              context.conceptData[context.currentConceptIndex].title
+            }`,
+            type: 'system',
+          },
+        ],
+      }),
+      showProblem: assign({
+        messages: (context) => [
+          ...context.messages,
+          { text: "Let's try a problem to assess your understanding.", type: 'system' },
+        ],
+      }),
+      moveToNextConcept: assign({
+        currentConceptIndex: (context) => context.currentConceptIndex + 1,
+      }),
+      promptNextConcept: assign({
+        messages: (context) => [
+          ...context.messages,
+          {
+            text: `Moving to the next concept: ${
+              context.conceptData[context.currentConceptIndex]?.title
+            }.`,
+            type: 'system',
+          },
+        ],
+      }),
+      completeTour: assign({
+        messages: (context) => [
+          ...context.messages,
+          { text: "You've completed the tour. Thank you for participating!", type: 'system' },
+        ],
+      }),
+      sendErrorMessage: assign({
+        messages: (context) => [
+          ...context.messages,
+          {
+            text: 'An error occurred while processing your request. Please try again later.',
+            type: 'system',
+          },
+        ],
+      }),
+    },
+    services: {
+      fetchLeafConcepts: (context, event) =>
+        getLeafConcepts(event.leafConceptUri).then((leafConceptLinks) =>
+          leafConceptLinks.leafConcepts.map((link) => ({
+            title: link.split('?').pop(),
+            concept_uri: link,
+            definition: null,
+            examples: [],
+            problems: [],
+          }))
+        ),
+      fetchLearningObjects: (context) => {
+        const currentConcept = context.conceptData[context.currentConceptIndex];
+        return Promise.all([
+          fetch(`https://stexmmt.mathhub.info/:sTeX/fragment?${currentConcept.concept_uri}`).then(
+            (res) => res.text()
+          ),
+          getLearningObjects([currentConcept.concept_uri]),
+        ]).then(([definition, learningObjects]) => ({
+          ...currentConcept,
+          definition,
+          examples: learningObjects.filter((obj) => obj.type === 'example'),
+          problems: learningObjects.filter((obj) => obj.type === 'problem'),
+        }));
+      },
+    },
+    guards: {
+      hasNextConcept: (context) => context.currentConceptIndex + 1 < context.conceptData.length,
+      noMoreConcepts: (context) => context.currentConceptIndex + 1 >= context.conceptData.length,
+    },
+  }
+);
 
 function GuidedTours() {
   const router = useRouter();
-  //   const title = router.query.id?.split('&title=')[1];
   const title = router.query.id?.match(/&title=([^&]*)/)?.[1] || '';
-
   const leafConceptUri = router.query.id?.split('&title=')[0];
-
-  const [currentConceptIndex, setCurrentConceptIndex] = useState(0);
-  const [messages, setMessages] = useState([]);
-  const messagesEndRef = useRef(null);
-  const [userResponse, setUserResponse] = useState(null);
-  const [shownDefinitions, setShownDefinitions] = useState({});
   const { mmtUrl } = useContext(ServerLinksContext);
+  const messagesEndRef = useRef(null);
 
-  const [conceptData, setConceptData] = useState([]);
-
-  const fetchLeafConcepts = async () => {
-    try {
-      const leafConceptLinks = await getLeafConcepts(leafConceptUri);
-
-      const conceptPromises = leafConceptLinks.leafConcepts.map(async (link) => {
-        const segments = link.split('?');
-        const conceptName = segments[segments.length - 1];
-
-        const [definitionResponse, learningObjectsResponse] = await Promise.all([
-          fetch(`https://stexmmt.mathhub.info/:sTeX/fragment?${link}`).then((res) => res.text()),
-          getLearningObjects([link]),
-        ]);
-        const problems = [];
-
-        const learningObjects = await Promise.all(
-          learningObjectsResponse.map(async (obj) => {
-            if (obj['type'] === 'problem') {
-              const problemHtml = await getProblemShtml(mmtUrl, obj['learning-object']);
-              problems.push(problemHtml);
-            }
-
-            return {
-              type: obj['type'],
-              url: obj['learning-object'],
-            };
-          })
-        );
-
-        return {
-          [conceptName]: {
-            title: conceptName,
-            concept_uri: link,
-            definition: definitionResponse,
-            learningObjects: learningObjects,
-            problems: problems,
-          },
-        };
-      });
-      const conceptDataArray = await Promise.all(conceptPromises);
-      setConceptData(conceptDataArray);
-    } catch (error) {
-      console.error('Error fetching leaf concepts:', error);
-    }
-  };
+  const [state, send] = useMachine(tourMachine, {
+    context: { mmtUrl },
+  });
 
   useEffect(() => {
-    fetchLeafConcepts();
-  }, [leafConceptUri]);
+    if (leafConceptUri) {
+      send({ type: 'LOAD_CONCEPTS', leafConceptUri });
+    }
+  }, [leafConceptUri, send]);
 
   useEffect(() => {
     if (messagesEndRef.current) {
       messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
     }
-  }, [messages]);
-
-  const handleUserResponse = async (response) => {
-    const currentConcept = Object.values(conceptData[currentConceptIndex])[0];
-    const currentConceptName = currentConcept.title;
-
-    setMessages((prevMessages) => [...prevMessages, { text: response, type: 'user' }]);
-    setUserResponse(response);
-
-    if (response === 'no') {
-      if (shownDefinitions[currentConceptName]) {
-        const nextConcept = Object.values(conceptData[currentConceptIndex + 1] || {})[0]?.title;
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          {
-            text: `Oops! We currently don't have more resources to help you. Moving to the next concept: <span style="color: #ed4b11;">${nextConcept}</span>.<br/>Do you feel comfortable with <span style="color: #1d9633;">${nextConcept}</span>?`,
-            type: 'system',
-          },
-        ]);
-        setCurrentConceptIndex((prevIndex) => prevIndex + 1);
-        setUserResponse(null);
-      } else {
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          {
-            text: `NO issue, we can do a small learning .Here is the definition of <span style="color: #c73730; font-size: 1.2rem; font-weight: bold;">${currentConceptName}</span>:<br/> ${currentConcept.definition}<br/>Do you understand it?`,
-            type: 'system',
-          },
-        ]);
-        setShownDefinitions((prevShown) => ({ ...prevShown, [currentConceptName]: true }));
-        setUserResponse(null);
-      }
-    } else if (response === 'yes') {
-      if (currentConceptIndex + 1 < conceptData.length) {
-        const nextConcept = Object.values(conceptData[currentConceptIndex + 1])[0].title;
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          {
-            text: `Okay, as you know the topic  <span style="color: #d629ce; font-size: 1.2rem; font-weight: bold;">${currentConceptName}</span>. We will move to the next concept: <span style="color: #1d9633;">${nextConcept}</span>.<br/>Do you feel comfortable with <span style="color: #ed4b11; font-size: 1.2rem; font-weight: bold;">${nextConcept}</span>?`,
-            type: 'system',
-          },
-        ]);
-        setCurrentConceptIndex((prevIndex) => prevIndex + 1);
-        setUserResponse(null);
-      } else {
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          {
-            text: `You've gone through all available concepts! Thank you for participating!`,
-            type: 'system',
-          },
-        ]);
-      }
-    } else if (response === 'not sure') {
-      // RECHECK THIS ONE
-      const learningObjects = currentConcept.learningObjects;
-      if (learningObjects.length > 0) {
-        const problemList = learningObjects.filter((obj) => obj.type === 'problem');
-        if (problemList.length > 0) {
-          const prob = await getProblemShtml(mmtUrl, problemList[0].url);
-          console.log('prNew', prob);
-
-          setMessages((prevMessages) => [
-            ...prevMessages,
-            {
-              text: `That's alright. Let's do a quick exercise to find out. Please try to answer the following problem(s):<br/>${prob}.</br> Do you get what the correct answer is?`,
-              type: 'system',
-            },
-          ]);
-        } else {
-          setMessages((prevMessages) => [
-            ...prevMessages,
-            {
-              text: `At the moment, we don't have any problems to assess your understanding. Instead, take a look at the definition below:<br/><span style='color: #d629ce; font-size: 1.2rem; font-weight: bold;'>${currentConcept.definition}</span>. Do you understand it?`,
-              type: 'system',
-            },
-          ]);
-        }
-      } else {
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          {
-            text: `Currently, we do not have any problems to check your understanding level. Therefore, have a look at its definition and move to the new concept:<br/><span style="color: #d629ce; font-size: 1.2rem; font-weight: bold;">${currentConcept.definition}</span>. Do you understand it?`,
-            type: 'system',
-          },
-        ]);
-      }
-      setUserResponse(null);
-    }
-  };
-
-  useEffect(() => {
-    if (conceptData.length > 1 && messages.length === 0) {
-      const currentConcept = Object.keys(conceptData[0])[0];
-      console.log('conceptData', conceptData);
-      setMessages((prevMessages) => [
-        ...prevMessages,
-        {
-          text: `
-            Happy to help you learn about <strong>${title}</strong>.<br/>
-            To understand <strong>${title}</strong>, it would be good to understand its prerequisites.<br/>
-            Based on your learner model, I think it will be a good idea to learn about <span style="color: #d629ce; font-size: 1.2rem; font-weight: bold;">${currentConcept}</span> first.<br/>
-            Do you feel comfortable with <span style="color: #d629ce; font-size: 1.2rem; font-weight: bold;">${currentConcept}</span>?`,
-          type: 'system',
-        },
-      ]);
-    }
-  }, [conceptData, messages]);
+  }, [state.context.messages]);
 
   return (
     <MainLayout title="Guided Tour">
@@ -191,7 +193,6 @@ function GuidedTours() {
           <span className={styles.welcomeText}>Welcome to the Guided Tour of </span>
           <span className={styles.dynamicTitle}>{title}</span>
         </h1>
-
         <Box
           display="flex"
           flexDirection="column"
@@ -202,7 +203,7 @@ function GuidedTours() {
           boxShadow={3}
           style={{ backgroundColor: '#f9f9f9', maxHeight: '60vh', overflowY: 'auto' }}
         >
-          {messages.map((msg, index) => (
+          {state.context.messages.map((msg, index) => (
             <Box
               key={index}
               display="flex"
@@ -219,7 +220,6 @@ function GuidedTours() {
                   maxWidth: '80%',
                   textAlign: msg.type === 'system' ? 'left' : 'right',
                 }}
-                className={`${styles.messageBox} ${styles.systemMessage}`}
               >
                 <Typography variant="body1" dangerouslySetInnerHTML={{ __html: msg.text }} />
               </Box>
@@ -228,19 +228,14 @@ function GuidedTours() {
           <div ref={messagesEndRef} />
         </Box>
 
-        {currentConceptIndex < conceptData.length && userResponse === null && (
-          <Box
-            display="flex"
-            justifyContent="flex-end"
-            mt={3}
-            className={`${styles.messageBox} ${styles.userMessage}`}
-          >
+        {state.matches('askingComfort') && (
+          <Box display="flex" justifyContent="flex-end" mt={3}>
             {['yes', 'no', 'not sure'].map((response) => (
               <Button
                 key={response}
                 variant="contained"
-                color={userResponse === response ? 'secondary' : 'primary'}
-                onClick={() => handleUserResponse(response)}
+                color={state.context.userResponse === response ? 'secondary' : 'primary'}
+                onClick={() => send(`RESPONSE_${response.toUpperCase()}`)}
                 style={{ margin: '0 5px' }}
               >
                 {response.charAt(0).toUpperCase() + response.slice(1)}
