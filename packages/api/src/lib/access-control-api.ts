7f0df51b5dc7e456886c9166359ad47d65908963
abchugh <achugh89@gmail.com>
2024-12-08 15:05:15 +0530

    deps-update 2/N
    @mui, emotion

--- packages/api/src/lib/access-control-api.ts
+++ packages/api/src/lib/access-control-api.ts
@@ -22,9 +22,9 @@ export async function getAcl(aclId: string): Promise<AccessControlList> {
   return resp.data as AccessControlList;
 }
 
-export async function getAclUserDetails(aclId : string):Promise<{fullName: string, userid:string}[]>{
+export async function getAclUserDetails(aclId: string) {
   const resp = await axios.get(`/api/access-control/get-acl-userdetails?id=${aclId}`);
-  return resp.data as {fullName: string,userid:string}[]
+  return resp.data as { fullName: string; userId: string }[];
 }
 
 export async function getCourseAcls(courseId: string, instanceId: string) {
