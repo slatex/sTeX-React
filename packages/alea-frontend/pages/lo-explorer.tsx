c65f0772d6dea2ee77fe25f17de2b6dff90fc360
abchugh <achugh89@gmail.com>
2025-01-05 05:24:15 +0530

    Update lo-explorer.tsx

--- packages/alea-frontend/pages/lo-explorer.tsx
+++ packages/alea-frontend/pages/lo-explorer.tsx
@@ -1,35 +1,6 @@
-import { Book, MicExternalOn, Quiz, SupervisedUserCircle } from '@mui/icons-material';
-import SchoolIcon from '@mui/icons-material/School';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import {
-  Autocomplete,
-  Box,
-  Button,
-  Checkbox,
-  Chip,
-  CircularProgress,
-  FormControl,
-  InputLabel,
-  ListItemText,
-  MenuItem,
-  Paper,
-  Select,
-  TextField,
-  Typography,
-} from '@mui/material';
-import { ALL_LO_TYPES, LoType, sparqlQuery } from '@stex-react/api';
-import { ServerLinksContext } from '@stex-react/stex-react-renderer';
-import {
-  capitalizeFirstLetter,
-  extractProjectIdAndFilepath,
-  localStore,
-  PRIMARY_COL,
-} from '@stex-react/utils';
-import React, { useContext, useEffect, useState } from 'react';
-import LoListDisplay from '../components/LoListDisplay';
-import LoCartModal, { CartItem } from '../components/LoCartModal';
-import MainLayout from '../layouts/MainLayout';
+import { Paper } from '@mui/material';
 import { LoExplorer } from '../components/lo-explorer';
+import MainLayout from '../layouts/MainLayout';
 
 const LoExplorerPage = () => {
   return (
