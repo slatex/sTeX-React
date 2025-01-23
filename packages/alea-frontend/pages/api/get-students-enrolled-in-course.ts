87dfdd15965524e5c01c8145d710a4bcb7595b46
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2025-01-23 14:20:26 +0530

    Update get-students-enrolled-in-course.ts

--- packages/alea-frontend/pages/api/get-students-enrolled-in-course.ts
+++ packages/alea-frontend/pages/api/get-students-enrolled-in-course.ts
@@ -1,6 +1,18 @@
+import { Action, CURRENT_TERM, getResourceId, ResourceName } from '@stex-react/utils';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { executeAndEndSet500OnError } from './comment-utils';
 import { getCourseEnrollmentAcl } from '../course-home/[courseId]';
+import { getCacheKey } from './acl-utils/acl-common-utils';
+import { returnAclIdForResourceIdAndActionId } from './acl-utils/resourceaccess-utils/resource-common-utils';
+
+const courseIds = ['ai-1', 'ai-2', 'lbs', 'iwgs-1', 'iwgs-2'];
+
+const instructorResourceAndAction = [
+  { resource: ResourceName.COURSE_NOTES, action: Action.MUTATE },
+  { resource: ResourceName.COURSE_QUIZ, action: Action.MUTATE },
+  { resource: ResourceName.COURSE_STUDY_BUDDY, action: Action.MODERATE },
+  { resource: ResourceName.COURSE_HOMEWORK, action: Action.MUTATE },
+  { resource: ResourceName.COURSE_ACCESS, action: Action.ACCESS_CONTROL },
+];
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -8,14 +20,43 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!courseId || !instanceId) {
       return res.status(400).json({ error: 'Missing courseId or instanceId in query parameters.' });
     }
-    const aclId = getCourseEnrollmentAcl(courseId as string, instanceId as string);
-    const students = await executeAndEndSet500OnError(
-      'SELECT memberUserId FROM ACLMembership WHERE parentACLId = ?',
-      [aclId],
-      res
+
+    const instructorResourceIdAndAction = courseIds.flatMap((course) =>
+      instructorResourceAndAction.map(({ resource, action }) => ({
+        resourceId: getResourceId(resource, {
+          courseId: course,
+          instanceId: CURRENT_TERM,
+        }),
+        action,
+      }))
     );
-    const userIds = students.map((row: { memberUserId: string }) => row.memberUserId);
-    return res.status(200).json(userIds);
+
+    const aclId = getCourseEnrollmentAcl(courseId as string, instanceId as string);
+    const enrolledPersons = await CACHE_STORE.getFromSet(getCacheKey(aclId));
+
+    const INSTRUCTOR_ACLS = Array.from(
+      new Set(
+        await Promise.all(
+          instructorResourceIdAndAction.map(async ({ resourceId, action }) => {
+            const aclId = await returnAclIdForResourceIdAndActionId(resourceId, action);
+            return aclId;
+          })
+        )
+      )
+    );
+
+    const instructors = (
+      await Promise.all(
+        INSTRUCTOR_ACLS.map(async (aclId) => {
+          const result = await CACHE_STORE.getFromSet(getCacheKey(aclId));
+          return result || [];
+        })
+      )
+    ).flat();
+
+    const uniqueInstructors = new Set(instructors);
+    const enrolledStudents = enrolledPersons.filter((person) => !uniqueInstructors.has(person));
+    return res.status(200).json(enrolledStudents);
   } catch (error) {
     console.error('Error fetching enrolled students:', error);
     return res.status(500).json({ error: 'Internal Server Error' });
