6d798f9dfec4b9e1a7735991636b593cd3799b5b
Abhishek Kumar <abhishek2021005@gmail.com>
2025-02-11 16:48:52 +0530

    slide navigation , marker positioning,clip Separation
    Signed-off-by: Abhishek Kumar <abhishek2021005@gmail.com>

--- packages/alea-frontend/components/VideoDisplay.tsx
+++ packages/alea-frontend/components/VideoDisplay.tsx
@@ -1,29 +1,46 @@
-import { FastForward, MusicNote } from '@mui/icons-material';
+import { FastForward, InfoOutlined, MusicNote } from '@mui/icons-material';
 import CheckIcon from '@mui/icons-material/Check';
 import SettingsIcon from '@mui/icons-material/Settings';
 import VideocamIcon from '@mui/icons-material/Videocam';
-import { Box, CircularProgress, Divider, IconButton, Menu, MenuItem, Tooltip } from '@mui/material';
-import { ClipDetails, SlideClipInfo } from '@stex-react/api';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
+  Paper,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import { ClipDetails, ClipInfo } from '@stex-react/api';
 import { localStore } from '@stex-react/utils';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+import '../styles/mediafile.module.scss';
+
+import { ClipData, setSlideNumAndSectionId } from '../pages/course-view/[courseId]';
 
 export default function SeekVideo({
   currentSlideClipInfo,
   setTimestampSec,
   setCurrentClipId,
 }: {
-  currentSlideClipInfo: SlideClipInfo;
+  currentSlideClipInfo: ClipInfo;
   setTimestampSec: React.Dispatch<React.SetStateAction<number>>;
   setCurrentClipId: Dispatch<SetStateAction<string>>;
 }) {
   const handleSeek = () => {
     if (currentSlideClipInfo) {
-      const { startTimeSec, clipId } = currentSlideClipInfo;
-      setCurrentClipId(clipId);
-      if (startTimeSec) {
-        setTimestampSec((prev) => (prev === startTimeSec ? prev + 0.001 : startTimeSec));
+      const { start_time, video_id } = currentSlideClipInfo;
+      setCurrentClipId(video_id);
+      if (start_time) {
+        setTimestampSec((prev) => (prev === start_time ? prev + 0.001 : start_time));
       } else setTimestampSec(0);
     }
   };
@@ -71,9 +88,9 @@ function ToggleResolution({
           {audioOnly ? <VideocamIcon /> : <MusicNote />}
         </Tooltip>
       </IconButton>
-      <Divider orientation="vertical" flexItem sx={{ mx: 1 }} />
       {!audioOnly && (
         <>
+          <Divider orientation="vertical" flexItem sx={{ mx: 1 }} />
           <IconButton onClick={(e) => setAnchorEl(e.currentTarget)}>
             <SettingsIcon />
           </IconButton>
@@ -110,56 +127,317 @@ function getVideoId(info: ClipDetails, needRes: number, availableRes: number[]) 
   return info[`r${res}`];
 }
 
-function MediaItem({
+interface Marker {
+  time: number;
+  label: string;
+  data: {
+    title?: string;
+    description?: string;
+    thumbnail?: string;
+    sectionId?: string;
+    slideIndex?: number;
+    ocr_slide_content?: string;
+  };
+}
+
+const MediaItem = ({
   audioOnly,
   videoId,
   sub,
   timestampSec,
+  markers,
+  onTimeUpdate,
 }: {
   audioOnly: boolean;
   videoId: string;
   sub?: string;
-  timestampSec: number;
-}) {
-  const mediaRef = useRef<HTMLVideoElement | HTMLAudioElement>(null);
+  timestampSec?: number;
+  markers: Marker[];
+  onTimeUpdate?: (slideIndex: number, sectionId: string) => void;
+}) => {
+  const playerRef = useRef<HTMLVideoElement | null>(null);
+  const videoPlayer = useRef<any>(null);
+  const [tooltip, setTooltip] = useState<string>('');
+  const [overlay, setOverlay] = useState<{ title: string; description: string } | null>(null);
+  const lastMarkerRef = useRef<string | null>(null);
+  const router = useRouter();
   useEffect(() => {
-    if (timestampSec) mediaRef.current.currentTime = timestampSec;
+    if (playerRef.current) {
+      videoPlayer.current = videojs(playerRef.current, {
+        controls: true,
+        preload: 'auto',
+        autoplay: true,
+        sources: [{ src: videoId, type: 'video/mp4' }],
+      });
+      const controlBar = playerRef.current.parentNode.querySelector(
+        '.vjs-control-bar'
+      ) as HTMLElement;
+      if (controlBar) {
+        controlBar.style.paddingBottom = '30px';
+        controlBar.style.paddingTop = '10px';
+        controlBar.style.position = 'absolute';
+        controlBar.style.zIndex = '9999';
+      }
+
+      const progressBar = playerRef.current.parentNode.querySelector(
+        '.vjs-progress-holder'
+      ) as HTMLElement;
+      if (progressBar) {
+        progressBar.style.marginTop = '20px';
+      }
+
+      const bigPlayButton = playerRef.current.parentNode.querySelector('.vjs-big-play-button');
+      if (bigPlayButton) {
+        const playIcon = bigPlayButton.querySelector('.vjs-icon-placeholder') as HTMLElement;
+        if (playIcon) {
+          playIcon.style.bottom = '5px';
+          playIcon.style.paddingRight = '25px';
+        }
+      }
+
+      const textTrackDisplay = playerRef.current.parentNode.querySelector(
+        '.vjs-text-track-display'
+      ) as HTMLElement;
+      if (textTrackDisplay) {
+        Object.assign(textTrackDisplay.style, {
+          insetBlock: '0px',
+          position: 'absolute',
+          top: '0',
+          left: '0',
+          right: '0',
+          bottom: '0',
+          margin: '0',
+          padding: '0',
+        });
+      }
+
+      videoPlayer.current.on('loadedmetadata', () => {
+        const progressHolder = videoPlayer.current.controlBar.progressControl.seekBar.el();
+        const videoDuration = videoPlayer.current.duration();
+        if (timestampSec) videoPlayer.current.currentTime(timestampSec);
+
+        markers.forEach((marker) => {
+          if (marker.time < videoDuration) {
+            const markerElement = document.createElement('div');
+            markerElement.className = 'custom-marker';
+            markerElement.dataset.label = marker.label;
+            markerElement.dataset.time = marker.time.toString();
+            markerElement.dataset.sectionId = marker.data.sectionId;
+            markerElement.dataset.slideIndex = String(marker.data.slideIndex);
+
+            Object.assign(markerElement.style, {
+              position: 'absolute',
+              top: '0',
+              width: '6px',
+              height: '100%',
+              backgroundColor: 'yellow',
+              left: `${(marker.time / videoDuration) * 100}%`,
+              zIndex: '10',
+              cursor: 'pointer',
+            });
+
+            markerElement.addEventListener('mouseenter', () =>
+              setTooltip(`${marker.label} - ${marker.time}s`)
+            );
+            markerElement.addEventListener('mouseleave', () => setTooltip(''));
+            markerElement.addEventListener('click', () =>
+              setOverlay({
+                title: marker.data.title ?? 'Untitled',
+                description: marker.data.description ?? 'No Description Available',
+              })
+            );
+
+            progressHolder.appendChild(markerElement);
+          }
+        });
+      });
+      videoPlayer.current.on('timeupdate', () => {
+        const currentTime = videoPlayer.current.currentTime();
+        const markers = Array.from(document.querySelectorAll('.custom-marker')) as HTMLElement[];
+        let latestMarker: HTMLElement | null = null;
+        for (const marker of markers) {
+          const markerTime = parseInt(marker.dataset.time, 10);
+          if (currentTime >= markerTime) {
+            marker.style.backgroundColor = 'green';
+            if (!latestMarker) {
+              latestMarker = marker;
+            }
+          } else {
+            marker.style.backgroundColor = 'yellow';
+          }
+        }
+
+        if (latestMarker) {
+          const sectionId = latestMarker.dataset.sectionId || '';
+          const slideIndex = latestMarker.dataset.slideIndex
+            ? parseInt(latestMarker.dataset.slideIndex, 10)
+            : 0;
+
+          const markerId = `${sectionId}-${slideIndex}`;
+
+          if (lastMarkerRef.current !== markerId) {
+            lastMarkerRef.current = markerId;
+            const safeSlideIndex = isNaN(slideIndex) || slideIndex === null ? 1 : slideIndex;
+            //causing the issue of sync video either directly will set sectionId and slideIndex here
+            //  or using onTimeUpdate
+            // setSlideNumAndSectionId(router, safeSlideIndex, sectionId);
+          }
+        }
+      });
+    }
+  }, [markers, timestampSec, videoId, onTimeUpdate]);
+  useEffect(() => {
+    if (videoPlayer.current && timestampSec !== undefined) {
+      videoPlayer.current.currentTime(timestampSec);
+    }
   }, [timestampSec]);
-  if (audioOnly) {
-    return (
-      <audio
-        autoPlay={true}
-        src={videoId}
-        preload="auto"
-        controls
-        onLoadedMetadata={() => {
-          if (timestampSec) mediaRef.current.currentTime = timestampSec;
-        }}
-        style={{ width: '100%', background: '#f1f3f4' }}
-        ref={mediaRef}
-      ></audio>
-    );
-  }
+  const handleMouseMove = (e: React.MouseEvent) => {
+    const progressBar = videoPlayer.current?.controlBar.progressControl.seekBar.el() as HTMLElement;
+    const rect = progressBar.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left;
+    const timeAtCursor = (mouseX / rect.width) * videoPlayer.current?.duration();
+
+    const closestMarker = markers.find((marker) => Math.abs(marker.time - timeAtCursor!) < 1);
+
+    if (closestMarker) {
+      setTooltip(`${closestMarker.label} - ${closestMarker.time}s`);
+    }
+  };
+
+  const handleMouseLeave = () => setTooltip('');
+
   return (
-    <video
-      autoPlay={true}
-      src={videoId}
-      crossOrigin="anonymous"
-      preload="auto"
-      controls
-      onLoadedMetadata={() => {
-        if (timestampSec) mediaRef.current.currentTime = timestampSec;
-      }}
-      style={{
-        width: '100%',
-        border: '1px solid black',
-        borderRadius: '5px',
-      }}
-      ref={mediaRef as any}
-    >
-      {sub && <track src={sub} label="English" kind="captions" srcLang="en-us" default></track>}
-    </video>
+    <div style={{ marginBottom: '7px', position: 'relative' }}>
+      <video
+        ref={playerRef}
+        className="video-js vjs-fluid vjs-styles=defaults vjs-big-play-centered"
+        style={{ border: '0.5px solid black', borderRadius: '8px' }}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+      >
+        {sub && <track src={sub} label="English" kind="captions" srcLang="en-us" default />}
+      </video>
+
+      {tooltip && (
+        <div
+          style={{
+            position: 'absolute',
+            top: '10px',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            color: '#fff',
+            padding: '5px 10px',
+            borderRadius: '4px',
+            zIndex: '100',
+          }}
+        >
+          {tooltip}
+        </div>
+      )}
+
+      {overlay && (
+        <div
+          style={{
+            position: 'absolute',
+            top: '20%',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            padding: '20px',
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            color: '#fff',
+            borderRadius: '8px',
+            zIndex: '100',
+            display: 'block',
+          }}
+        >
+          <h3>{overlay.title}</h3>
+          <p>{overlay.description}</p>
+          <button onClick={() => setOverlay(null)} style={{ background: '#f00', color: '#fff' }}>
+            Close
+          </button>
+        </div>
+      )}
+    </div>
   );
+};
+
+function DraggableOverlay({ showOverlay, setShowOverlay, data }) {
+  const overlayRef = useRef(null);
+  const [position, setPosition] = useState({ x: 100, y: 100 });
+  const [dragging, setDragging] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+  const formatTime = (seconds) => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secondsLeft = Math.floor(seconds % 60);
+
+    if (hours > 0) {
+      return `${hours} hr ${minutes} min ${secondsLeft} sec`;
+    } else {
+      return `${minutes} min ${secondsLeft} sec`;
+    }
+  };
+  const handleMouseDown = (e) => {
+    setDragging(true);
+    setOffset({
+      x: e.clientX - position.x,
+      y: e.clientY - position.y,
+    });
+  };
+
+  const handleMouseMove = (e) => {
+    if (!dragging) return;
+    setPosition({
+      x: e.clientX - offset.x,
+      y: e.clientY - offset.y,
+    });
+  };
+
+  const handleMouseUp = () => setDragging(false);
+  const { parentClipId, currentSlideClipInfo } = data;
+
+  return showOverlay ? (
+    <Box
+      ref={overlayRef}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      style={{
+        position: 'fixed',
+        backgroundColor: 'rgba(0, 0, 0, 0.2)',
+        top: `${position.y}px`,
+        left: `${position.x}px`,
+        width: '300px',
+        padding: '10px',
+        cursor: 'grab',
+        zIndex: 1000,
+      }}
+    >
+      <Paper elevation={3} sx={{ p: 2, position: 'relative', bgcolor: 'rgba(255, 255, 255, 0.9)' }}>
+        <IconButton
+          onClick={() => setShowOverlay(false)}
+          sx={{ position: 'absolute', right: 5, top: 5, zIndex: 1 }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+          🔍 Video Infos:
+        </Typography>
+        <Typography variant="body2">Parent Clip Id: {parentClipId}</Typography>
+        <Typography variant="body1" sx={{ fontWeight: 'bold', mt: 2 }}>
+          Current slide Clip Infos:
+        </Typography>
+        <Typography variant="body2">Clip ID: {currentSlideClipInfo?.video_id}</Typography>
+        <Typography variant="body2">
+          Start Time: {formatTime(currentSlideClipInfo?.start_time)}
+        </Typography>
+        <Typography variant="body2">
+          End Time: {formatTime(currentSlideClipInfo?.end_time)}
+        </Typography>
+      </Paper>
+    </Box>
+  ) : null;
 }
 
 export function VideoDisplay({
@@ -169,20 +447,44 @@ export function VideoDisplay({
   setTimestampSec,
   currentSlideClipInfo,
   audioOnly,
+  videoExtractedData,
+  onTimeUpdate,
 }: {
   clipId: string;
   setCurrentClipId: Dispatch<SetStateAction<string>>;
   timestampSec?: number;
   setTimestampSec?: Dispatch<SetStateAction<number>>;
-  currentSlideClipInfo?: SlideClipInfo;
+  currentSlideClipInfo?: ClipInfo;
   audioOnly: boolean;
+  videoExtractedData?: {
+    [timestampSec: number]: ClipData;
+  };
+  onTimeUpdate?: (slideIndex: number, sectionId: string) => void;
 }) {
   const [resolution, setResolution] = useState(720);
   const [clipDetails, setClipDetails] = useState(undefined as ClipDetails);
   const availableRes = getAvailableRes(clipDetails);
   const videoId = getVideoId(clipDetails, resolution, availableRes);
   const [isLoading, setIsLoading] = useState(true);
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [reveal, setReveal] = useState(false);
   const router = useRouter();
+  const extractedValues = Object.values(videoExtractedData || {});
+  const markers = extractedValues
+    .filter((item: any) => {
+      return (item.sectionId || '').trim() !== '' && item.slideIndex !== null;
+    })
+    .map((item: any) => ({
+      time: Math.floor(item.start_time ?? 0),
+      label: item.title || 'Untitled',
+      data: {
+        thumbnail: item.thumbnail || null,
+        ocr_slide_content: item.ocr_slide_content || null,
+        sectionId: item.sectionId,
+        slideIndex: item.slideIndex,
+      },
+    }));
+
   useEffect(() => {
     if (!clipId) {
       setIsLoading(false);
@@ -198,38 +500,82 @@ export function VideoDisplay({
   useEffect(() => {
     setResolution(+(localStore?.getItem('defaultResolution') || '720'));
   }, []);
+  const handleKeyPress = (e) => {
+    if (e.key === 'Shift') setReveal(true);
+  };
+
+  const handleKeyRelease = (e) => {
+    if (e.key === 'Shift') setReveal(false);
+  };
   if (isLoading) return <CircularProgress />;
   if (!videoId) return <i>Video not available for this section</i>;
   return (
     <>
-      <MediaItem
-        videoId={videoId}
-        timestampSec={timestampSec}
-        audioOnly={audioOnly}
-        sub={clipDetails?.sub}
-      />
-      <Box sx={{ display: 'flex', m: '-4px 0 5px' }}>
-        <ToggleResolution
+      <Box
+        style={{
+          width: '100%',
+          position: 'relative',
+          zIndex: 1,
+        }}
+      >
+        <MediaItem
+          videoId={videoId}
+          timestampSec={timestampSec}
           audioOnly={audioOnly}
-          setAudioOnly={(v: boolean) => {
-            const audioOnlyStr = v.toString();
-            localStore?.setItem('audioOnly', audioOnlyStr);
-            router.query.audioOnly = audioOnlyStr;
-            router.replace(router);
-          }}
-          resolution={resolution}
-          setResolution={(res: number) => {
-            setResolution(res);
-            localStore?.setItem('defaultResolution', res.toString());
-          }}
-          availableResolutions={availableRes}
+          sub={clipDetails?.sub}
+          markers={markers}
+          onTimeUpdate={onTimeUpdate}
         />
-        <SeekVideo
-          currentSlideClipInfo={currentSlideClipInfo}
-          setTimestampSec={setTimestampSec}
-          setCurrentClipId={setCurrentClipId}
-        />
+        <Box sx={{ display: 'flex', m: '-4px 0 5px' }}>
+          <ToggleResolution
+            audioOnly={audioOnly}
+            setAudioOnly={(v: boolean) => {
+              const audioOnlyStr = v.toString();
+              localStore?.setItem('audioOnly', audioOnlyStr);
+              router.query.audioOnly = audioOnlyStr;
+              router.replace(router);
+            }}
+            resolution={resolution}
+            setResolution={(res: number) => {
+              setResolution(res);
+              localStore?.setItem('defaultResolution', res.toString());
+            }}
+            availableResolutions={availableRes}
+          />
+          <SeekVideo
+            currentSlideClipInfo={currentSlideClipInfo}
+            setTimestampSec={setTimestampSec}
+            setCurrentClipId={setCurrentClipId}
+          />
+          <Box
+            onKeyDown={handleKeyPress}
+            onKeyUp={handleKeyRelease}
+            tabIndex={0}
+            sx={{
+              display: 'inline-block',
+            }}
+          >
+            <Button
+              onDoubleClick={() => setShowOverlay((prev) => !prev)}
+              style={{
+                cursor: 'pointer',
+                opacity: reveal ? 1 : 0,
+                pointerEvents: reveal ? 'auto' : 'none',
+              }}
+            >
+              <InfoOutlined />
+            </Button>
+          </Box>
+          {showOverlay && (
+            <DraggableOverlay
+              showOverlay={showOverlay}
+              setShowOverlay={setShowOverlay}
+              data={{ parentClipId: clipId, currentSlideClipInfo: currentSlideClipInfo }}
+            />
+          )}
+        </Box>
       </Box>
+      <div style={{ height: '20px' }} />
     </>
   );
 }
--- packages/alea-frontend/pages/api/get-section-info/[courseId].ts
+++ packages/alea-frontend/pages/api/get-section-info/[courseId].ts
@@ -1,7 +1,7 @@
 import {
+  ClipInfo,
   SectionInfo,
   SectionsAPIData,
-  SlideClipInfo,
   getCourseInfo,
   getDocumentSections,
 } from '@stex-react/api';
@@ -10,22 +10,23 @@ import { convert } from 'html-to-text';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getCoverageData } from '../get-coverage-timeline';
 import { readdir, readFile } from 'fs/promises';
+import { getSlideCounts } from '../get-slide-counts/[courseId]';
 const CACHE_EXPIRY_TIME = 60 * 60 * 1000;
-const videoToSlidesMap: Record<string, any> = {};
+export const CACHED_VIDEO_SLIDESMAP: Record<string, any> = {};
 let cacheRefreshTime: number | undefined = undefined;
 let cachePromise: Promise<void> | null = null;
 
-async function populateVideoToSlidesMap() {
+export async function populateVideoToSlidesMap() {
   const dirPath = process.env.VIDEO_TO_SLIDES_MAP_DIR;
   if (!dirPath) return;
   const files = await readdir(dirPath);
   for (const file of files) {
-    if (file.endsWith('_processed_slides.json')) {
-      const courseId = file.replace('_processed_slides.json', '');
+    if (file.endsWith('_extracted_content.json')) {
+      const courseId = file.replace('_extracted_content.json', '');
       const filePath = `${dirPath}/${file}`;
       const fileData = await readFile(filePath, 'utf-8');
       const data = JSON.parse(fileData);
-      videoToSlidesMap[courseId] = data;
+      CACHED_VIDEO_SLIDESMAP[courseId] = data;
     }
   }
   cacheRefreshTime = Date.now();
@@ -48,9 +49,9 @@ async function refreshCache() {
 async function getVideoToSlidesMap(courseId: string) {
   const isCacheExpired = !cacheRefreshTime || Date.now() > cacheRefreshTime + CACHE_EXPIRY_TIME;
   if (isCacheExpired && !cachePromise) {
-    refreshCache();
+    await refreshCache();
   }
-  return videoToSlidesMap[courseId];
+  return CACHED_VIDEO_SLIDESMAP[courseId];
 }
 
 function getAllSections(data: SectionsAPIData, level = 0): SectionInfo | SectionInfo[] {
@@ -94,27 +95,54 @@ export function addVideoInfo(sections: SectionInfo[], snaps: CoverageSnap[]) {
   return;
 }
 
-function addClipInfo(allSections: SectionInfo[], jsonData: any[]) {
-  const clipDataMap: {
-    [sectionId: string]: SlideClipInfo[];
-  } = {};
-  jsonData.forEach((entry) => {
-    const { sectionId, start_time, end_time, video_id } = entry;
-    if (!clipDataMap[sectionId]) {
-      clipDataMap[sectionId] = [];
-    }
-    if (start_time !== undefined && end_time !== undefined) {
-      clipDataMap[sectionId].push({
-        clipId: video_id,
-        startTimeSec: start_time,
-        endTimeSec: end_time,
+function addClipInfo(
+  allSections: SectionInfo[],
+  slideCounts: { [key: string]: number },
+  jsonData: any
+) {
+  const clipDataMap: { [sectionId: string]: { [slideIndex: number]: ClipInfo[] } } = {};
+
+  Object.entries(jsonData).forEach(
+    ([videoId, videoData]: [string, { extracted_content: { [timeStamp: number]: ClipInfo } }]) => {
+      const extractedContent: { [timeStamp: number]: ClipInfo } = videoData.extracted_content;
+      if (!extractedContent) return;
+      Object.entries(extractedContent).forEach(([timeStamp, clipData]) => {
+        const { sectionId, slideIndex } = clipData;
+        if (!sectionId || slideIndex === null) return;
+        if (!clipDataMap[sectionId]) {
+          const totalSlides = slideCounts[sectionId] || 0;
+          clipDataMap[sectionId] = {};
+          for (let i = 1; i <= totalSlides; i++) {
+            clipDataMap[sectionId][i] = [];
+          }
+        }
+        if (!clipDataMap[sectionId][slideIndex]) {
+          clipDataMap[sectionId][slideIndex] = [];
+        }
+
+        clipDataMap[sectionId][slideIndex].push({
+          video_id: videoId,
+          start_time: clipData.start_time,
+          end_time: clipData.end_time,
+          //donot remove ocr_slide_content and slideContent
+          // (kept it for debugging purpose when needed)
+          // ocr_slide_content: clipData.ocr_slide_content,
+          title: clipData.title,
+          thumbnail: clipData.thumbnail,
+          // slideContent: clipData.slideContent,
+        });
       });
     }
-  });
+  );
   function processSections(sections: SectionInfo[]) {
     sections.forEach((section) => {
       if (clipDataMap[section.id]) {
         section.clipInfo = clipDataMap[section.id];
+      } else {
+        section.clipInfo = {};
+        for (let i = 1; i <= (slideCounts[section.id] || 0); i++) {
+          section.clipInfo[i] = [];
+        }
       }
 
       if (section.children && section.children.length > 0) {
@@ -142,8 +170,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const coverageData = getCoverageData()[courseId].filter(snap=>snap.sectionName);
   if (coverageData?.length) addVideoInfo(allSections, coverageData);
   const videoSlides = await getVideoToSlidesMap(courseId);
-  if (videoSlides) {
-    addClipInfo(allSections, videoSlides);
+  const slideCounts = await getSlideCounts(courseId, res);
+  if (videoSlides && Object.keys(videoSlides).length > 0) {
+    addClipInfo(allSections, slideCounts, videoSlides);
   }
   res.status(200).send(allSections);
 }
