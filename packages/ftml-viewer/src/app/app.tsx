a21ea2296882d3765826fa79bb8d396ed6f3409c
abchugh <achugh89@gmail.com>
2025-02-13 15:15:02 +0530

    ftml-viewer

---
+++ packages/ftml-viewer/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+import { Box } from '@mui/material';
+
+export function App() {
+  return (
+    <div>
+      <Box >Empty1</Box>
+    </div>
+  );
+}
+
+export default App;
