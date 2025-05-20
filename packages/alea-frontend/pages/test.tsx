f93f1786266068d33345fa34d7086447e0c73d77
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2025-05-20 19:10:50 +0530

    Update test.tsx

--- packages/alea-frontend/pages/test.tsx
+++ packages/alea-frontend/pages/test.tsx
@@ -4,6 +4,9 @@ import { useState } from 'react';
 const Test = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [status, setStatus] = useState('loggedOut');
+  function test(){
+    return "test";
+  }
   return <Box>Test</Box>;
 };
 
