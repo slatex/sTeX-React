d89c99979c63af2fae6fa863d092709066f077a8
Keerthan K <keerthank.is20@sahyadri.edu.in>
2025-06-03 14:39:34 +0530

    removed files from main

--- packages/nodejs-scripts/src/main.ts
+++ packages/nodejs-scripts/src/main.ts
@@ -1,20 +1,9 @@
-import { addUserIdToAccessControlList } from './addUserIdToAccessControlList';
-import { endSemSummary } from './endSemSummary';
 import { quizLmsInfoWriter } from './quizLmsInfoWriter';
-import { recorrectQuizzes } from './recorrectQuizzes';
 
 switch (process.env.SCRIPT_NAME) {
-  case 'endSemSummary':
-    endSemSummary();
-    break;
+
   case 'quizLmsInfoWriter':
     quizLmsInfoWriter();
-    break;
-  case 'recorrectQuizzes':
-    recorrectQuizzes();
-    break;
-  case 'addUserIdToAccessControlList':
-    addUserIdToAccessControlList();
     break;
   default:
     console.log('Invalid script name');
